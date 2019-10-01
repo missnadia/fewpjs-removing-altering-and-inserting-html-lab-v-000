aa1173a17befd1b97aeeef3ee6006f108d7fdcc9
Nadia Lee <miss.nadia.lee@gmail.com>
2019-10-01 19:23:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ document.querySelector("main#main").remove()
 
 let h = document.createElement("h1")
 h.id = "victory"
+document.body.appendChild(h)
 let newHeader = document.querySelector("h1#victory")
 newHeader.innerHTML = "is the champion"
